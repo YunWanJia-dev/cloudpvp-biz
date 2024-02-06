876d982cc04620bdd707c3429c06b93cb63ba25d
sheip9 <sheip9@outlook.com>
2024-02-06 16:53:43 +0800

    aa

--- cloudpvp-auth/src/main/java/me/ywj/cloudpvp/auth/CloudpvpAuthApplication.java
+++ cloudpvp-auth/src/main/java/me/ywj/cloudpvp/auth/CloudpvpAuthApplication.java
@@ -2,8 +2,15 @@ package me.ywj.cloudpvp.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+/**
+ * CloudpvpAuthApplication
+ * 启动类
+ * @author sheip9
+ */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CloudpvpAuthApplication {
 
     public static void main(String[] args) {
