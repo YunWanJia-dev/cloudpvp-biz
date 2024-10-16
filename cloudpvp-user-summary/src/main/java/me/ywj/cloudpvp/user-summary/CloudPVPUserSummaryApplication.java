8acb535ef0af66d54642c058eef2a24b7b81ee4b
sheip9 <sheip9@outlook.com>
2024-10-16 12:03:15 +0800

    Renaming module

--- cloudpvp-user-summary/src/main/java/me/ywj/cloudpvp/user-summary/CloudPVPUserSummaryApplication.java
+++ cloudpvp-user-summary/src/main/java/me/ywj/cloudpvp/user-summary/CloudPVPUserSummaryApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CloudUserSummaryApplication {
+public class CloudPVPUserSummaryApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CloudUserSummaryApplication.class, args);
+		SpringApplication.run(CloudPVPUserSummaryApplication.class, args);
 	}
 
 }
