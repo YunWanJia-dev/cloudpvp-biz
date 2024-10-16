c183ff396d7ce86c4a874285ed37702f2b517f28
sheip9 <sheip9@outlook.com>
2024-10-16 16:34:27 +0800

    new module

---
+++ cloudpvp-state/src/main/java/me/ywj/cloudpvp/cloudpvpstate/CloudpvpStateApplication.java
@@ -0,0 +1,13 @@
+package me.ywj.cloudpvp.cloudpvpstate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudpvpStateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudpvpStateApplication.class, args);
+	}
+
+}
---
+++ cloudpvp-state/src/test/java/me/ywj/cloudpvp/cloudpvpstate/CloudpvpStateApplicationTests.java
@@ -0,0 +1,13 @@
+package me.ywj.cloudpvp.cloudpvpstate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudpvpStateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
