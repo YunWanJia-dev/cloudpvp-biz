b5b6814d397b21841f1dae813dbab05b594dd2e3
sheip9 <sheip9@outlook.com>
2024-10-16 17:34:59 +0800

    Steam Urls

---
+++ cloudpvp-core/src/main/java/me/ywj/cloudpvp/core/constant/steam/SteamApiUrl.kt
@@ -0,0 +1,12 @@
+package me.ywj.cloudpvp.core.constant.steam
+
+/**
+ * SteamApiUrl
+ *
+ * @author sheip9
+ * @since 2024/10/16 16:50
+ */
+object SteamApiUrl {
+    const val steamchina = "https://api.steamchina.com/"
+    const val steampowered = "http://api.steampowered.com/"
+}
