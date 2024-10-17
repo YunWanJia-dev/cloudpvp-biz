92ae32e6b99478997cc2eb4de2080cd287641e3f
sheip9 <sheip9@outlook.com>
2024-10-17 13:29:46 +0800

    redis configure

---
+++ cloudpvp-state/src/main/java/me/ywj/cloudpvp/cloudpvpstate/configure/RedisConfigure.java
@@ -0,0 +1,41 @@
+package me.ywj.cloudpvp.cloudpvpstate.configure;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * RedisConfigure
+ *
+ * @author sheip9
+ * @since 2024/2/28 19:16
+ */
+@Configuration
+public class RedisConfigure {
+    @Bean
+    public RedisTemplate<String, ?> redisTemplate(RedisConnectionFactory connectionFactory) {
+        final RedisSerializer<?> serializer = new GenericJackson2JsonRedisSerializer();
+        RedisTemplate<String, ?> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        // 使用StringRedisSerializer来序列化和反序列化redis的key值
+        template.setKeySerializer(new StringRedisSerializer());
+        // 使用fastjson来序列化和反序列化redis的value值
+        template.setValueSerializer(serializer);
+        // 设置hash key 和value序列化模式
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setHashValueSerializer(serializer);
+        return template;
+    }
+    @Bean
+    public RedisMessageListenerContainer redisContainer(RedisConnectionFactory factory) {
+        final RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(factory);
+        return container;
+    }
+}
