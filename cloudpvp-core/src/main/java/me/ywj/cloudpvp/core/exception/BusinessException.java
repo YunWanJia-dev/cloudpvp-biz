9df635cad0bd0cf5ae5c9c9044558e1aa93ff773
sheip9 <sheip9@outlook.com>
2024-10-24 17:18:12 +0800

    BusinessException

---
+++ cloudpvp-core/src/main/java/me/ywj/cloudpvp/core/exception/BusinessException.java
@@ -0,0 +1,10 @@
+package me.ywj.cloudpvp.core.exception;
+
+/**
+ * BusinessException
+ *  业务异常
+ * @author sheip9
+ * @since 2024/10/24 17:14
+ */
+public abstract class BusinessException extends Exception {
+}
