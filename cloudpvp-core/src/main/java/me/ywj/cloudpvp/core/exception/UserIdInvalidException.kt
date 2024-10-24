1b722515d70f7b5d0027f3e5003ea491b9718e13
sheip9 <sheip9@outlook.com>
2024-10-24 17:18:29 +0800

    change extending to BusinessException

--- cloudpvp-core/src/main/java/me/ywj/cloudpvp/core/exception/UserIdInvalidException.kt
+++ cloudpvp-core/src/main/java/me/ywj/cloudpvp/core/exception/UserIdInvalidException.kt
@@ -2,9 +2,9 @@ package me.ywj.cloudpvp.core.exception
 
 /**
  * UserIdInvalidException
- *
+ *  用户id无效异常
  * @author sheip9
  * @since 2024/10/19 00:13
  */
-class UserIdInvalidException : Exception() {
+class UserIdInvalidException : BusinessException() {
 }
