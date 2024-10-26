211bb392e3eb8fd1a49b8c86110aa69deabb6463
sheip9 <sheip9@outlook.com>
2024-10-26 20:12:18 +0800

    State: defining player's current lobby id

--- cloudpvp-state/src/main/java/me/ywj/cloudpvp/state/entity/PlayerState.kt
+++ cloudpvp-state/src/main/java/me/ywj/cloudpvp/state/entity/PlayerState.kt
@@ -1,6 +1,7 @@
 package me.ywj.cloudpvp.state.entity
 
 import me.ywj.cloudpvp.core.entity.BasicPlayer
+import me.ywj.cloudpvp.core.type.LobbyId
 import me.ywj.cloudpvp.core.type.SteamID64
 import me.ywj.cloudpvp.state.constant.StateEnum
 import org.springframework.data.annotation.Id
@@ -15,4 +16,5 @@ import org.springframework.data.redis.core.RedisHash
 @RedisHash("people")
 class PlayerState(@Id override val steamID64 : SteamID64) : BasicPlayer(steamID64)  {
     var state : StateEnum = StateEnum.UNKNOWN
+    var currentLobbyId : LobbyId? = null
 }
