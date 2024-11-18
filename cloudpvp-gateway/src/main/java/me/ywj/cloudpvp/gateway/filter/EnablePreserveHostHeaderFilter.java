69a85b0963868b5b4ac2879eeaf12c229a2342db
sheip9 <sheip9@outlook.com>
2024-11-18 17:06:20 +0800

    add EnablePreserveHostHeaderFilter

---
+++ cloudpvp-gateway/src/main/java/me/ywj/cloudpvp/gateway/filter/EnablePreserveHostHeaderFilter.java
@@ -0,0 +1,27 @@
+package me.ywj.cloudpvp.gateway.filter;
+
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.cloud.gateway.filter.factory.PreserveHostHeaderGatewayFilterFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+/**
+ * EnablePreserveHostHeaderFilter
+ *
+ * @author sheip9
+ * @since 2024/11/18 15:26
+ */
+@Component
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class EnablePreserveHostHeaderFilter implements GlobalFilter {
+    private final PreserveHostHeaderGatewayFilterFactory preserveHostHeaderGatewayFilterFactory;
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        return preserveHostHeaderGatewayFilterFactory.apply(null).filter(exchange, chain);
+    }
+}
