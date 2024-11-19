f01c293d60ff046d13b4268cad49db2ca6cd0124
sheip9 <sheip9@outlook.com>
2024-11-19 16:40:50 +0800

    HttpUtils: 完善注释

--- cloudpvp-core/src/main/java/me/ywj/cloudpvp/core/utils/HttpUtils.java
+++ cloudpvp-core/src/main/java/me/ywj/cloudpvp/core/utils/HttpUtils.java
@@ -6,15 +6,15 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 /**
- * UriUtils
- *
+ * HttpUtils
+ *  网络请求工具类
  * @author sheip9
  * @since 2024/11/14 16:20
  */
 public class HttpUtils {
     private final HttpClient httpClient;
     private final HttpRequest baseRequest;
-
+    
     public HttpUtils(HttpRequest baseRequest) {
         this.httpClient = HttpClient.newHttpClient();
         this.baseRequest = baseRequest;
@@ -28,24 +28,53 @@ public class HttpUtils {
         return this.httpClient.send(request, HttpResponse.BodyHandlers.ofString());
     }
 
+    /**
+     * 发起GET请求
+     * @param path 路径
+     * @return HttpResponse
+     * @throws Exception 异常
+     */
     public HttpResponse<String> get(String path) throws Exception {
         var req = newRequest(path).GET().build();
         return send(req);
     }
 
+    /**
+     * 发起GET请求
+     * @return HttpResponse
+     * @throws Exception 异常
+     */
     public HttpResponse<String> get() throws Exception {
         return get("");
     }
 
+    /**
+     * 发起POST请求
+     * @param path 路径
+     * @param body 内容体
+     * @return HttpResponse
+     * @throws Exception 异常
+     */
     public HttpResponse<String> post(String path, Object body) throws Exception {
         var req = newRequest(path).POST(HttpRequest.BodyPublishers.ofString(JacksonUtils.serialize(body))).build();
         return send(req);
     }
 
+    /**
+     * 发起POST请求
+     * @param body 内容体
+     * @return HttpResponse
+     * @throws Exception 异常
+     */
     public HttpResponse<String> post(Object body) throws Exception {
         return post("", body);
     }
 
+    /**
+     * 发起POST请求
+     * @return HttpResponse
+     * @throws Exception 异常
+     */
     public HttpResponse<String> post() throws Exception {
         return post("", null);
     }
