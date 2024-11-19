8fe560c9b12b983f298343e18140e9ed4863915b
sheip9 <sheip9@outlook.com>
2024-11-19 19:21:46 +0800

    Gateway: fixing cors configuration

---
+++ cloudpvp-gateway/src/main/java/me/ywj/cloudpvp/gateway/configuration/CorsFilterConfiguration.java
@@ -0,0 +1,32 @@
+package me.ywj.cloudpvp.gateway.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+/**
+ * CorsWebFilterBean
+ *
+ * @author sheip9
+ * @since 2024/11/19 19:05
+ */
+@Configuration
+public class CorsFilterConfiguration {
+    @Bean
+    CorsWebFilter corsFilter() {
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(false);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
+    }
+}
