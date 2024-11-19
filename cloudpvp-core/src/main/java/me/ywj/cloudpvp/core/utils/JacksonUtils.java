d3c979e070399f26f68e8f6156689466614a505a
sheip9 <sheip9@outlook.com>
2024-11-19 16:43:38 +0800

    JacksonUtils: 完善注释

--- cloudpvp-core/src/main/java/me/ywj/cloudpvp/core/utils/JacksonUtils.java
+++ cloudpvp-core/src/main/java/me/ywj/cloudpvp/core/utils/JacksonUtils.java
@@ -9,15 +9,21 @@ import java.io.IOException;
 
 /**
  * JacksonUtils
- *
+ * 序列化工具类
  * @author sheip9
  * @since 2024/10/25 16:26
  */
 public class JacksonUtils {
+    /**
+     * Jackson实例
+     */
     public final static ObjectMapper INSTANCE = createInstance();
     
     private JacksonUtils() {}
-    
+
+    /**
+     * 创建和设定实例
+     */
     private static ObjectMapper createInstance() {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.configure(JsonParser.Feature.INCLUDE_SOURCE_IN_LOCATION, true);
@@ -25,11 +31,24 @@ public class JacksonUtils {
         objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         return objectMapper;
     }
-    
+
+    /**
+     * 序列化对象
+     * @param o 要序列化的对象
+     * @return 序列化后的JSON字符串
+     * @throws IOException 异常
+     */
     public static String serialize(Object o) throws IOException {
         return INSTANCE.writeValueAsString(o);
     }
-    
+
+    /**
+     * 反序列化对象
+     * @param json 要用于反序列化的JSON字符串
+     * @param clazz 反序列化的目标对象
+     * @return 反序列化后的对象
+     * @throws IOException 异常
+     */
     public static <T> T deserialize(String json, Class<T> clazz) throws IOException {
         return INSTANCE.readValue(json, clazz);
     }
