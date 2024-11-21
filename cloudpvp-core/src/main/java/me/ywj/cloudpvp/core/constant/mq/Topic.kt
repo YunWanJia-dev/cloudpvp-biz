027db1d4112a2cdecb5c5f6bc23c8919aa4b199b
sheip9 <sheip9@outlook.com>
2024-11-21 20:39:22 +0800

    Module user-summary: 引入RocketMQ

---
+++ cloudpvp-core/src/main/java/me/ywj/cloudpvp/core/constant/mq/Topic.kt
@@ -0,0 +1,11 @@
+package me.ywj.cloudpvp.core.constant.mq
+
+/**
+ * Topic
+ *
+ * @author sheip9
+ * @since 2024/11/21 15:25
+ */
+object Topic {
+    const val UPDATE_PLAYER_PROFILE = "update-player-profile"
+}
