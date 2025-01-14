0bccc3e90c4b0a3332dc91bb67f89ff8a3a5b015
sheip9 <sheip9@outlook.com>
2025-01-14 12:02:42 +0800

    a comment error

--- cloudpvp-state/src/main/java/me/ywj/cloudpvp/state/configure/RedisConfigure.java
+++ cloudpvp-state/src/main/java/me/ywj/cloudpvp/state/configure/RedisConfigure.java
@@ -28,7 +28,7 @@ public class RedisConfigure {
         template.setConnectionFactory(connectionFactory);
         // 使用StringRedisSerializer来序列化和反序列化redis的key值
         template.setKeySerializer(new StringRedisSerializer());
-        // 使用fastjson来序列化和反序列化redis的value值
+        // 使用jackson来序列化和反序列化redis的value值
         template.setValueSerializer(SERIALIZER);
         // 设置hash key 和value序列化模式
         template.setHashKeySerializer(new StringRedisSerializer());
