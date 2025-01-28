74de66c59cfc85c415dc1a94b360b1ff801546d0
sheip9 <sheip9@outlook.com>
2025-01-28 23:42:11 +0800

    FeishuProperty: 完善注释

--- cloudpvp-beans/src/main/java/me/ywj/cloudpvp/beans/property/FeishuProperty.java
+++ cloudpvp-beans/src/main/java/me/ywj/cloudpvp/beans/property/FeishuProperty.java
@@ -2,9 +2,21 @@ package me.ywj.cloudpvp.beans.property;
 
 import me.ywj.cloudpvp.core.model.configuration.FeishuWebhookConfiguration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Configuration;
 
-@Component
+/**
+ * FeishuProperty
+ * 飞书Webhook配置类
+ * <p>
+ * Example：
+ * <pre>
+ * cloudpvp:
+ *   feishu:
+ *     uri: URI_HERE
+ *     sign: SIGN_HERE (NULLABLE)
+ * </pre>
+ */
+@Configuration
 @ConfigurationProperties("cloudpvp.feishu")
 public class FeishuProperty extends FeishuWebhookConfiguration {
 }
