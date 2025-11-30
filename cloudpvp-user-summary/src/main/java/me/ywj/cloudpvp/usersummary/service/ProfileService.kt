74fc8e8103cbcf0b28502fc67a164ed2f0efe8e9
sheip9 <sheip9@outlook.com>
2025-11-30 15:49:55 +0800

    remove rocketmq

--- cloudpvp-user-summary/src/main/java/me/ywj/cloudpvp/usersummary/service/ProfileService.kt
+++ cloudpvp-user-summary/src/main/java/me/ywj/cloudpvp/usersummary/service/ProfileService.kt
@@ -1,13 +1,7 @@
 package me.ywj.cloudpvp.usersummary.service
 
-import me.ywj.cloudpvp.core.constant.mq.Topic
 import me.ywj.cloudpvp.core.constant.steam.SteamUser
 import me.ywj.cloudpvp.core.entity.PlayerProfile
-import org.apache.rocketmq.client.producer.SendCallback
-import org.apache.rocketmq.client.producer.SendResult
-import org.apache.rocketmq.spring.core.RocketMQTemplate
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.messaging.support.MessageBuilder
 import org.springframework.stereotype.Service
 import reactor.core.publisher.Flux
 
@@ -18,9 +12,7 @@ import reactor.core.publisher.Flux
  * @since 2024/10/20 18:30
  */
 @Service
-class ProfileService @Autowired constructor(val rocketMQTemplate: RocketMQTemplate) {
-    val mqCallback = MQSendCallback()
-
+class ProfileService () {
     fun getProfile(ids: ArrayList<Long>): List<PlayerProfile?>? {
         //先暂时这样 嘻嘻
         //TODO: 完成玩家数据从数据库查询
@@ -41,20 +33,6 @@ class ProfileService @Autowired constructor(val rocketMQTemplate: RocketMQTempla
     }
 
     fun requestUpdateProfile(id: Long) {
-        MessageBuilder.withPayload<Long>(id)
-        rocketMQTemplate.asyncSend(
-            Topic.UPDATE_PLAYER_PROFILE, MessageBuilder.withPayload<Long>(id).build(),
-            mqCallback
-        )
-    }
-}
 
-class MQSendCallback : SendCallback {
-    override fun onSuccess(p0: SendResult?) {
     }
-
-    override fun onException(p0: Throwable?) {
-        p0?.printStackTrace()
-    }
-
 }
