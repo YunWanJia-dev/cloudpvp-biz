0621a3899c1e10471c3820dc4202185d7e07102a
sheip9 <sheip9@outlook.com>
2026-05-15 17:05:51 +0800

    feat(user-summary): 接入 Redis 玩家资料缓存并按需回源 Steam API

---
+++ cloudpvp-user-summary/src/main/java/me/ywj/cloudpvp/usersummary/repository/PlayerProfileRepository.kt
@@ -0,0 +1,13 @@
+package me.ywj.cloudpvp.usersummary.repository
+
+import me.ywj.cloudpvp.usersummary.entity.PlayerProfileCache
+import org.springframework.data.repository.CrudRepository
+
+/**
+ * PlayerProfileRepository
+ * 玩家资料 Redis 缓存仓库。
+ *
+ * @author sheip9
+ * @since 2026/5/15 16:53
+ */
+interface PlayerProfileRepository : CrudRepository<PlayerProfileCache, Long>
